9a9f0e248c4db1a2fc21ab168d5a4074a83e2576
jgerarias <47396560+jgerarias@users.noreply.github.com>
2019-02-20 14:54:19 -0300

    ingreso programación UTN
    tp hasta el tres completo

--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -4,7 +4,12 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+var temperatura;
+var calculo;
+ 
+    temperatura = parseFloat (document.getElementById("Temperatura").value);
+    calculo = (temperatura-32)*5/9;
+        alert (temperatura +" Fahrenheit son " + calculo + " centigrados");
 }
 
 function CentigradosFahrenheit () 
